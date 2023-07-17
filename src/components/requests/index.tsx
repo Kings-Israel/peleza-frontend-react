6efb86eff0efed0bff1e1b76453eeaa3bb722755
robina_99 <robinarasanga@gmail.com>
2023-07-17 10:56:37 +0300

    changes

--- src/components/requests/index.tsx
+++ src/components/requests/index.tsx
@@ -635,11 +635,7 @@ class DataTable extends Component<{
         </MuiPickersUtilsProvider>
       
             </div>
-            <div className='col-md-4'>
-            
-  
-      
-            </div>
+           
       
             </div>
             <div className="row"> 
